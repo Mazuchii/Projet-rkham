86e93bcf5ba469c7ea21284ee857be59f157ae46
Caind430 <jasserbenhassine1@gmail.com>
2025-01-08 14:06:08 +0000

    Initial commit or updated project files

--- catalogue/settings.py
+++ catalogue/settings.py
@@ -131,9 +131,13 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/5.1/howto/static-files/
 
-STATIC_URL = '/static/'
-STATICFILES_DIRS = [BASE_DIR / "static"]
-STATIC_ROOT = BASE_DIR / "staticfiles"
+TATIC_URL = '/static/'  # URL to access static files
+
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')  # Path where collectstatic gathers files
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static'),  # Your static source directory
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
